9b0a3c8b3f41d66c8a81cb47f7cc21b8cad5e13a
Petrik Diak <diak@petrik.hu>
2024-10-09 14:29:11 +0200

    elv mukodik

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,72 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Post } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import myDTO from './myDTO.dto';
+import { Res } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+
+
+  @Post()
+  getFormsData(@Res() res, @Body() MyDto: myDTO) {
+    console.log(MyDto);
+
+    let error = [];
+
+    if (MyDto.username.trim().length < 1) {
+      error.push("Hibás felhasználónév");
+    }
+
+    if (!/\d{8}-\d{8}-\d{8}/.test(MyDto.creditcard) && !/\d{24}/.test(MyDto.creditcard) && !/\d{8}-\d{8}/.test(MyDto.creditcard) && !/\d{16}/.test(MyDto.creditcard)) {
+      error.push("Hibás bankszámlaszám");
+    }
+
+    if (typeof (MyDto.eula) == "undefined") {
+      error.push("Nem fogadtad el a felhasználási feltételeket");
+    }
+
+    if (error.length < 1) {
+      const fs = require('node:fs');
+      console.log("ok");
+
+      let tmp = MyDto.username+";"+MyDto.username+";";
+      fs.writeFile('../public/myCSV.csv', tmp, err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.error("Success");
+        }
+      });
+
+
+      res.redirect('mindenis');
+    }
+
+    console.log(error);
+    res.render('index', {
+      errors: error,
+      username: MyDto.username
+    })
+  }
+
+  @Get('mindenis')
+  getMindenis() {
+    return "OK"
+  }
+
+
 
   @Get()
   @Render('index')
-  getHello() {
+  getForm() {
     return {
-      message: this.appService.getHello()
+      username: "",
+      errors: []
     };
   }
+
 }
---
+++ src/myDTO.dto.ts
@@ -0,0 +1,5 @@
+export default class myDTO {
+  username: string;
+  creditcard: string;
+  eula: boolean;
+}
